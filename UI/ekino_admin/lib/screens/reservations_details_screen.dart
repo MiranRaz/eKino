9220c7706d08695f4ba2d8e2de7a145aa1156919
Miran Raznatovic <raznatovicmiran@gmail.com>
2024-02-23 11:35:22 +0100

    WIP seat picker
    WIP seat picker

--- UI/ekino_admin/lib/screens/reservations_details_screen.dart
+++ UI/ekino_admin/lib/screens/reservations_details_screen.dart
@@ -2,11 +2,11 @@ import 'package:ekino_admin/models/projection.dart';
 import 'package:ekino_admin/models/reservation.dart';
 import 'package:ekino_admin/models/user.dart';
 import 'package:ekino_admin/providers/projections_provider.dart';
+import 'package:ekino_admin/providers/reservation_provider.dart';
 import 'package:ekino_admin/providers/users_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 
 class ReservationDetailsScreen extends StatefulWidget {
   final Reservation? reservation;
@@ -24,13 +24,23 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
   late Map<String, dynamic> _initialValue;
   late UsersProvider _usersProvider;
   late ProjectionsProvider _projectionsProvider;
+  late ReservationProvider _reservationProvider;
   List<Users>? _usersList;
   List<Projection>? _projectionsList;
   bool isLoading = true;
+  late List<List<bool>> seats;
+  List<String> selectedRows = [];
+  List<int> selectedColumns = [];
+
+  List<Reservation>? allReservations; // Added to store all reservations
 
   @override
   void initState() {
     super.initState();
+    _usersProvider = context.read<UsersProvider>();
+    _projectionsProvider = context.read<ProjectionsProvider>();
+    _reservationProvider = context.read<ReservationProvider>();
+
     _initialValue = {
       "userId": widget.reservation?.userId?.toString(),
       "projectionId": widget.reservation?.projectionId.toString(),
@@ -38,10 +48,8 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
       "column": widget.reservation?.column ?? '',
       "numTickets": widget.reservation?.numTickets ?? '',
     };
-
-    _usersProvider = context.read<UsersProvider>();
-    _projectionsProvider = context.read<ProjectionsProvider>();
     initForm();
+    seats = List.generate(8, (_) => List.generate(8, (_) => false));
   }
 
   Future<void> initForm() async {
@@ -49,9 +57,12 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
     _usersList = usersResult.result;
     final projectionsResult = await _projectionsProvider.get();
     _projectionsList = projectionsResult.result;
+    final reservationsResult = await _reservationProvider.get();
+    allReservations = reservationsResult.result;
 
     setState(() {
       isLoading = false;
+      seats = List.generate(8, (_) => List.generate(8, (_) => false));
     });
   }
 
@@ -71,12 +82,23 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: isLoading ? CircularProgressIndicator() : _buildForm(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : _buildForm(),
+              SizedBox(height: 20),
+              _buildSeatPicker(),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  FormBuilder _buildForm() {
+  Widget _buildForm() {
     return FormBuilder(
       key: _formKey,
       initialValue: _initialValue,
@@ -118,6 +140,7 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
             name: 'numTickets',
             decoration: InputDecoration(labelText: "Number of Tickets"),
           ),
+          const SizedBox(height: 24),
           ElevatedButton(
             onPressed: () async {
               _formKey.currentState?.save();
@@ -161,5 +184,140 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
         );
       },
     );
+  }
+
+  Widget _buildSeatPicker() {
+    return Container(
+      padding: EdgeInsets.all(50),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Movie screen',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 16),
+          CustomPaint(
+            size: Size(1200, 10), // Adjust size as needed
+            painter: ScreenPainter(),
+          ),
+          SizedBox(height: 48),
+          const Text(
+            'Select a Seat',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8, // Reduced number of columns
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8,
+              childAspectRatio: 5, // Adjust aspect ratio as needed
+            ),
+            itemCount: 40, // Reduced total number of seats
+            itemBuilder: (context, index) {
+              final row = index ~/ 8; // Row index
+              final column = index % 8; // Column index
+              final seatRow = String.fromCharCode(65 + row);
+              final seatColumn = column + 1;
+
+              // Check if the seat belongs to any reservation
+              final isReserved = allReservations?.any((reservation) {
+                    final rowList = reservation.row?.split(',') ?? [];
+                    final columnList = reservation.column?.split(',') ?? [];
+                    if (rowList.isNotEmpty &&
+                        columnList.isNotEmpty &&
+                        rowList.length == columnList.length) {
+                      return rowList.asMap().entries.any((entry) {
+                        final rowIndex = entry.key;
+                        final rowValue = entry.value;
+                        final columnValue =
+                            int.tryParse(columnList.elementAt(rowIndex));
+                        return seatRow == rowValue && seatColumn == columnValue;
+                      });
+                    }
+                    return false;
+                  }) ??
+                  false;
+
+              final currentUserReservation = widget.reservation;
+              final currentUserReserved = currentUserReservation != null &&
+                  currentUserReservation.row
+                          ?.split(',')
+                          .asMap()
+                          .entries
+                          .any((entry) {
+                        final rowIndex = entry.key;
+                        final rowValue = entry.value;
+                        final columnList =
+                            currentUserReservation.column?.split(',') ?? [];
+                        if (columnList.isNotEmpty &&
+                            rowIndex < columnList.length) {
+                          final columnValue =
+                              int.tryParse(columnList[rowIndex]);
+                          return seatRow == rowValue &&
+                              seatColumn == columnValue;
+                        }
+                        return false;
+                      }) ==
+                      true;
+
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    // You can add logic here if needed
+                  });
+                },
+                child: Container(
+                  color: currentUserReserved
+                      ? Colors.green // Current user's reservation
+                      : (isReserved
+                          ? Colors.red
+                          : Colors.grey), // Other reservations
+                  child: Center(
+                    child: Text(
+                      '${String.fromCharCode(65 + row)}${column + 1}',
+                      style: TextStyle(
+                        color: currentUserReserved || isReserved
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScreenPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 12.0 // Adjust stroke width as needed
+      ..style = PaintingStyle.stroke;
+
+    Offset controlPoint1 = Offset(size.width * 0.25, size.height * 0.75);
+    Offset controlPoint2 = Offset(size.width * 0.75, size.height * 0.75);
+
+    Path path = Path()
+      ..moveTo(0, size.height)
+      ..quadraticBezierTo(controlPoint1.dx, controlPoint1.dy, size.width / 2,
+          size.height * 0.25)
+      ..quadraticBezierTo(
+          controlPoint2.dx, controlPoint2.dy, size.width, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
